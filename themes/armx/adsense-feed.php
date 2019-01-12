6233cc6a0231daac836065f1b683d603054fbdb0
jerryzcxisme <jerryzcxisme@gmail.com>
2019-01-12 21:21:12 +0800

    fix adsense feed

--- themes/armx/adsense-feed.php
+++ themes/armx/adsense-feed.php
@@ -1,15 +1,16 @@
 <?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
 <article class="post">
 <div class="card post-box clearfix">
-  <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-  <ins class="adsbygoogle"
-       style="display:block"
-       data-ad-format="fluid"
-       data-ad-layout-key="-gj-2k+b6-8r-cb"
-       data-ad-client="ca-pub-4664451695421699"
-       data-ad-slot="8071859111"></ins>
-  <script>
-       (adsbygoogle = window.adsbygoogle || []).push({});
-  </script>
+        <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+        <ins class="adsbygoogle"
+             style="display:block"
+             data-ad-format="fluid"
+             data-ad-layout-key="-gi-1t+9k-8v-ao"
+             data-ad-client="ca-pub-4664451695421699"
+             data-ad-slot="8071859111"></ins>
+        <script>
+             (adsbygoogle = window.adsbygoogle || []).push({});
+        </script>
+
 </div>
 </article>
